d77000b426e4c4047dffc0839c2a75a5a617eb32
WalterMarkerian <waltermarkerian@gmail.com>
2023-03-08 18:10:28 -0300

    docker-compose servicio MySql + application.properties y conexion establecida

--- src/main/java/com/example/learning/videojuegos/domain/Videojuego.java
+++ src/main/java/com/example/learning/videojuegos/domain/Videojuego.java
@@ -1,24 +1,40 @@
 package com.example.learning.videojuegos.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class Videojuego {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     private String nombre;
     private String descripcion;
     private String imagenUrl;
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getDescripcion() {
         return descripcion;
     }
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
     public String getImagenUrl() {
         return imagenUrl;
     }
+
     public void setImagenUrl(String imagenUrl) {
         this.imagenUrl = imagenUrl;
     }
